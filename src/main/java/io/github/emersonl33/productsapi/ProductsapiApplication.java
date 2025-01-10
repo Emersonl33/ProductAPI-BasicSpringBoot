8fe099d2fc86a2dd0188e4932183bb8ba8f169d4
emersonl33 <emersonl33@hotmail.com>
2025-01-10 02:00:54 -0300

    H2Create

---
+++ src/main/java/io/github/emersonl33/productsapi/ProductsapiApplication.java
@@ -0,0 +1,14 @@
+package io.github.emersonl33.productsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsapiApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/emersonl33/productsapi/ProductsapiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.emersonl33.productsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
